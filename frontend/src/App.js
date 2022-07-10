ba96ec6c1d8d37cf6bebcb262f58f541c67d2810
Liang <72845312+Liang417@users.noreply.github.com>
2022-07-10 22:03:36 +0800

    List-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product_info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
